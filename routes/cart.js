b6568cc8ecfd3b2118d001a81229ecef7b2b8c9f
sajnaparveen <sajna.platosys.com>
2022-05-06 11:14:31 +0530

    addtocart

--- routes/cart.js
+++ routes/cart.js
@@ -11,12 +11,13 @@ router.post("/createcart", async (req, res) => {
     const savedCart = await newCart.save();
     return res.status(200).json({ 'status': 'success', "message": "Order created!", "result": savedCart })
   }else{
-    let ttf =  order[0].products
-  let updateOrder =ttf.push(req.body.products);
-  console.log("hvhjsx",ttf)
-  const jbdsb = await findOneAndUpdate({userUuid:order[0].userUuid},{products:ttf},{new:true}).exec();
-  console.log("hvhjsx",jbdsb)
-    return res.status(200).json({ 'status': 'success', "message": "Order Updataeds!",result:jbdsb })
+    let takenOrder =  order[0].products
+  let updateOrder =takenOrder.push(req.body.products);
+  console.log("takenOrder",takenOrder)
+  console.log("userUuid:order[0].userUuid", order[0].userUuid)
+  const findUpdateOrder = await cart.findOneAndUpdate({userUuid:order[0].userUuid},{products:takenOrder},{new:true}).exec();
+  console.log("findUpdateOrder",findUpdateOrder)
+    return res.status(200).json({ 'status': 'success', "message": "Order Updataeds!",result:findUpdateOrder })
 
   }
    
